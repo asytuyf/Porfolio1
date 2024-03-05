4463016dacf305db1c5cf22e632aae61d0541732
Cyandied <Thea-Johansen@outlook.dk>
2024-03-05 15:35:09 +0100

    Tests and documentation
    More tests written
    BachelorProgramme class commented/documented

--- Code/src/BachelorProgramme.java
+++ Code/src/BachelorProgramme.java
@@ -11,21 +11,30 @@ public class BachelorProgramme {
     }
 
     public boolean valid(){
+        //A simple size check is done first, because if the programme has too many/too few
+        //Courses or projects, we can avoid a lot of computing since we already know it is an invalid programme
         if((courses.size() < 16 || courses.size() > 18) && projects.size() != 6){
             return false;
         }
+        //We store total ECTs points for the whole program here
         int totalEcts = 0;
 
+        //We store the count for each course type here
         int totalBasicCourses = 0;
         int totalSubModCourses = 0;
         int totalElectiveCourses = 0;
 
+        //We store the count for each project type here
         int totalBasicProjects = 0;
         int totalSubModProjects = 0;
         int totalBachelorProjects = 0;
 
+        //We iterate through all courses in the programme
+        //A complex for loop is chosen to improve readability within the loop
+        //An alternative could be storing courses[i] in a Course course variable in the loop
         for(Course course : courses){
             totalEcts += course.getEcts();
+            //Variable to count duplicates
             int dupesFound = 0;
             switch (course.getType()){
                 case BASIC -> totalBasicCourses++;
@@ -35,6 +44,9 @@ public class BachelorProgramme {
                     return false;
                 }
             }
+            //We compare the course to every other course to ensure that no two courses are the same
+            //A caveat here is that we will inevitably compare the course to itself
+            //Therefore, the amount of dupes tolerated are 1, not 0
             for(Course courseCompare : courses){
                 if(course.sameActivity(courseCompare)){
                     dupesFound++;
@@ -65,15 +77,20 @@ public class BachelorProgramme {
             }
         }
 
+        //Long strings of logic saved to variables for readability
+        //We have consistently checked for the negative, so here we also check if the course and project combination is invalid
         boolean coursesInvalid = (totalBasicCourses != 8 || (totalSubModCourses < 6 || totalSubModCourses > 8) || totalElectiveCourses != 2);
         boolean projectsInvalid = (totalBasicProjects != 3 || totalSubModProjects != 2 || totalBachelorProjects != 1);
 
         if (totalEcts != 180 || coursesInvalid || projectsInvalid){
             return false;
         }
+        //Since we consistently check for the negative, we can safely return true here, because the programme has passed every check
         return true;
     }
 
+    //Instead of having a check within the addActivity method to sort the given activity into the courses or projects list
+    //we overload the method, saving us an if or switch statement
     public void addActivity(Course course){
         courses.add(course);
     }
--- Code/src/Tests.java
+++ Code/src/Tests.java
@@ -2,6 +2,23 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//        From here on, everything other than what is mentioned should be valid.
+//
+//        No projects -> invalid
+//        No courses  -> invalid
+//
+//        All below should have the correct amount of ECTs:
+//
+//        Not enough courses  -> invalid
+//
+//        No basic courses -> invalid
+//        No subject mod courses -> invalid
+//        No Elective courses -> invalid
+//
+//        No basic projects -> invalid
+//        No subject mod projects -> invalid
+//        No bachelor project -> invalid
 
 public class Tests {
     @Test
@@ -79,6 +96,72 @@ public class Tests {
     }
 
     @Test
+    void duplicateCourse() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+        programme.addActivity(new BasicCourse(5,"a"));
+        programme.addActivity(new BasicCourse(5,"a"));
+        programme.addActivity(new BasicCourse(5,"c"));
+        programme.addActivity(new BasicCourse(5,"d"));
+        programme.addActivity(new BasicCourse(5,"e"));
+        programme.addActivity(new BasicCourse(5,"f"));
+        programme.addActivity(new BasicCourse(5,"g"));
+        programme.addActivity(new BasicCourse(5,"h"));
+
+        programme.addActivity(new SubjectModuleCourse(5,"aa"));
+        programme.addActivity(new SubjectModuleCourse(5,"bb"));
+        programme.addActivity(new SubjectModuleCourse(5,"cc"));
+        programme.addActivity(new SubjectModuleCourse(5,"dd"));
+        programme.addActivity(new SubjectModuleCourse(10,"ee"));
+        programme.addActivity(new SubjectModuleCourse(10,"ff"));
+
+        programme.addActivity(new ElectiveCourse(5,"aaa"));
+        programme.addActivity(new ElectiveCourse(5,"bbb"));
+
+        programme.addActivity(new BasicProject("aaaa"));
+        programme.addActivity(new BasicProject("bbbb"));
+        programme.addActivity(new BasicProject("cccc"));
+
+        programme.addActivity(new SubejctModuleProject("aaaaa"));
+        programme.addActivity(new SubejctModuleProject("bbbbb"));
+
+        programme.addActivity(new BachelorProject("aaaaaa"));
+        assertFalse(programme.valid());
+    }
+
+    @Test
+    void  duplicateProject() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+        programme.addActivity(new BasicCourse(5,"a"));
+        programme.addActivity(new BasicCourse(5,"b"));
+        programme.addActivity(new BasicCourse(5,"c"));
+        programme.addActivity(new BasicCourse(5,"d"));
+        programme.addActivity(new BasicCourse(5,"e"));
+        programme.addActivity(new BasicCourse(5,"f"));
+        programme.addActivity(new BasicCourse(5,"g"));
+        programme.addActivity(new BasicCourse(5,"h"));
+
+        programme.addActivity(new SubjectModuleCourse(5,"aa"));
+        programme.addActivity(new SubjectModuleCourse(5,"bb"));
+        programme.addActivity(new SubjectModuleCourse(5,"cc"));
+        programme.addActivity(new SubjectModuleCourse(5,"dd"));
+        programme.addActivity(new SubjectModuleCourse(10,"ee"));
+        programme.addActivity(new SubjectModuleCourse(10,"ff"));
+
+        programme.addActivity(new ElectiveCourse(5,"aaa"));
+        programme.addActivity(new ElectiveCourse(5,"bbb"));
+
+        programme.addActivity(new BasicProject("aaaa"));
+        programme.addActivity(new BasicProject("aaaa"));
+        programme.addActivity(new BasicProject("cccc"));
+
+        programme.addActivity(new SubejctModuleProject("aaaaa"));
+        programme.addActivity(new SubejctModuleProject("bbbbb"));
+
+        programme.addActivity(new BachelorProject("aaaaaa"));
+        assertFalse(programme.valid());
+    }
+
+    @Test
     void NotEnoughECTS() throws Exception {
         BachelorProgramme programme = new BachelorProgramme();
         programme.addActivity(new BasicCourse(5,"a"));
@@ -97,6 +180,40 @@ public class Tests {
         programme.addActivity(new SubjectModuleCourse(5,"ee"));
         programme.addActivity(new SubjectModuleCourse(5,"ff"));
         programme.addActivity(new SubjectModuleCourse(5,"gg"));
+
+        programme.addActivity(new ElectiveCourse(5,"aaa"));
+        programme.addActivity(new ElectiveCourse(5,"bbb"));
+
+        programme.addActivity(new BasicProject("aaaa"));
+        programme.addActivity(new BasicProject("bbbb"));
+        programme.addActivity(new BasicProject("cccc"));
+
+        programme.addActivity(new SubejctModuleProject("aaaaa"));
+        programme.addActivity(new SubejctModuleProject("bbbbb"));
+
+        programme.addActivity(new BachelorProject("aaaaaa"));
+        assertFalse(programme.valid());
+    }
+
+    @Test
+    void TooManyECTS() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+        programme.addActivity(new BasicCourse(5,"a"));
+        programme.addActivity(new BasicCourse(5,"b"));
+        programme.addActivity(new BasicCourse(5,"c"));
+        programme.addActivity(new BasicCourse(5,"d"));
+        programme.addActivity(new BasicCourse(5,"e"));
+        programme.addActivity(new BasicCourse(5,"f"));
+        programme.addActivity(new BasicCourse(5,"g"));
+        programme.addActivity(new BasicCourse(5,"h"));
+
+        programme.addActivity(new SubjectModuleCourse(5,"aa"));
+        programme.addActivity(new SubjectModuleCourse(5,"bb"));
+        programme.addActivity(new SubjectModuleCourse(5,"cc"));
+        programme.addActivity(new SubjectModuleCourse(5,"dd"));
+        programme.addActivity(new SubjectModuleCourse(10,"ee"));
+        programme.addActivity(new SubjectModuleCourse(10,"ff"));
+        programme.addActivity(new SubjectModuleCourse(10,"gg"));
 
         programme.addActivity(new ElectiveCourse(5,"aaa"));
         programme.addActivity(new ElectiveCourse(5,"bbb"));
@@ -182,7 +299,7 @@ public class Tests {
     }
 
     @Test
-    void TooManyECTS() throws Exception {
+    void TooFewCourses() throws Exception {
         BachelorProgramme programme = new BachelorProgramme();
         programme.addActivity(new BasicCourse(5,"a"));
         programme.addActivity(new BasicCourse(5,"b"));
@@ -195,11 +312,9 @@ public class Tests {
 
         programme.addActivity(new SubjectModuleCourse(5,"aa"));
         programme.addActivity(new SubjectModuleCourse(5,"bb"));
-        programme.addActivity(new SubjectModuleCourse(5,"cc"));
-        programme.addActivity(new SubjectModuleCourse(5,"dd"));
+        programme.addActivity(new SubjectModuleCourse(10,"dd"));
         programme.addActivity(new SubjectModuleCourse(10,"ee"));
         programme.addActivity(new SubjectModuleCourse(10,"ff"));
-        programme.addActivity(new SubjectModuleCourse(10,"gg"));
 
         programme.addActivity(new ElectiveCourse(5,"aaa"));
         programme.addActivity(new ElectiveCourse(5,"bbb"));
@@ -215,4 +330,213 @@ public class Tests {
         assertFalse(programme.valid());
     }
 
+    @Test
+    void AllBasicProjects() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+        programme.addActivity(new BasicCourse(5,"a"));
+        programme.addActivity(new BasicCourse(5,"b"));
+        programme.addActivity(new BasicCourse(5,"c"));
+        programme.addActivity(new BasicCourse(5,"d"));
+        programme.addActivity(new BasicCourse(5,"e"));
+        programme.addActivity(new BasicCourse(5,"f"));
+        programme.addActivity(new BasicCourse(5,"g"));
+        programme.addActivity(new BasicCourse(5,"h"));
+
+        programme.addActivity(new SubjectModuleCourse(5,"aa"));
+        programme.addActivity(new SubjectModuleCourse(5,"bb"));
+        programme.addActivity(new SubjectModuleCourse(5,"cc"));
+        programme.addActivity(new SubjectModuleCourse(5,"dd"));
+        programme.addActivity(new SubjectModuleCourse(5,"ee"));
+        programme.addActivity(new SubjectModuleCourse(5,"ff"));
+        programme.addActivity(new SubjectModuleCourse(5,"gg"));
+        programme.addActivity(new SubjectModuleCourse(5,"hh"));
+
+        programme.addActivity(new ElectiveCourse(5,"aaa"));
+        programme.addActivity(new ElectiveCourse(5,"bbb"));
+
+        programme.addActivity(new BasicProject("aaaa"));
+        programme.addActivity(new BasicProject("bbbb"));
+        programme.addActivity(new BasicProject("cccc"));
+
+        programme.addActivity(new BasicProject("aaaaa"));
+        programme.addActivity(new BasicProject("bbbbb"));
+
+        programme.addActivity(new BasicProject("aaaaaa"));
+        assertFalse(programme.valid());
+    }
+
+    @Test
+    void AllSubModProjects() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+        programme.addActivity(new BasicCourse(5,"a"));
+        programme.addActivity(new BasicCourse(5,"b"));
+        programme.addActivity(new BasicCourse(5,"c"));
+        programme.addActivity(new BasicCourse(5,"d"));
+        programme.addActivity(new BasicCourse(5,"e"));
+        programme.addActivity(new BasicCourse(5,"f"));
+        programme.addActivity(new BasicCourse(5,"g"));
+        programme.addActivity(new BasicCourse(5,"h"));
+
+        programme.addActivity(new SubjectModuleCourse(5,"aa"));
+        programme.addActivity(new SubjectModuleCourse(5,"bb"));
+        programme.addActivity(new SubjectModuleCourse(5,"cc"));
+        programme.addActivity(new SubjectModuleCourse(5,"dd"));
+        programme.addActivity(new SubjectModuleCourse(5,"ee"));
+        programme.addActivity(new SubjectModuleCourse(5,"ff"));
+        programme.addActivity(new SubjectModuleCourse(5,"gg"));
+        programme.addActivity(new SubjectModuleCourse(5,"hh"));
+
+        programme.addActivity(new ElectiveCourse(5,"aaa"));
+        programme.addActivity(new ElectiveCourse(5,"bbb"));
+
+        programme.addActivity(new SubejctModuleProject("aaaa"));
+        programme.addActivity(new SubejctModuleProject("bbbb"));
+        programme.addActivity(new SubejctModuleProject("cccc"));
+
+        programme.addActivity(new SubejctModuleProject("aaaaa"));
+        programme.addActivity(new SubejctModuleProject("bbbbb"));
+
+        programme.addActivity(new SubejctModuleProject("aaaaaa"));
+        assertFalse(programme.valid());
+    }
+
+    @Test
+    void AllBachelorProjects() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+        programme.addActivity(new BasicCourse(5,"a"));
+        programme.addActivity(new BasicCourse(5,"b"));
+        programme.addActivity(new BasicCourse(5,"c"));
+        programme.addActivity(new BasicCourse(5,"d"));
+        programme.addActivity(new BasicCourse(5,"e"));
+        programme.addActivity(new BasicCourse(5,"f"));
+        programme.addActivity(new BasicCourse(5,"g"));
+        programme.addActivity(new BasicCourse(5,"h"));
+
+        programme.addActivity(new SubjectModuleCourse(5,"aa"));
+        programme.addActivity(new SubjectModuleCourse(5,"bb"));
+        programme.addActivity(new SubjectModuleCourse(5,"cc"));
+        programme.addActivity(new SubjectModuleCourse(5,"dd"));
+        programme.addActivity(new SubjectModuleCourse(5,"ee"));
+        programme.addActivity(new SubjectModuleCourse(5,"ff"));
+        programme.addActivity(new SubjectModuleCourse(5,"gg"));
+        programme.addActivity(new SubjectModuleCourse(5,"hh"));
+
+        programme.addActivity(new ElectiveCourse(5,"aaa"));
+        programme.addActivity(new ElectiveCourse(5,"bbb"));
+
+        programme.addActivity(new BachelorProject("aaaa"));
+        programme.addActivity(new BachelorProject("bbbb"));
+        programme.addActivity(new BachelorProject("cccc"));
+
+        programme.addActivity(new BachelorProject("aaaaa"));
+        programme.addActivity(new BachelorProject("bbbbb"));
+
+        programme.addActivity(new BachelorProject("aaaaaa"));
+        assertFalse(programme.valid());
+    }
+
+    @Test
+    void AllBasicCourses() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+        programme.addActivity(new BasicCourse(5,"a"));
+        programme.addActivity(new BasicCourse(5,"b"));
+        programme.addActivity(new BasicCourse(5,"c"));
+        programme.addActivity(new BasicCourse(5,"d"));
+        programme.addActivity(new BasicCourse(5,"e"));
+        programme.addActivity(new BasicCourse(5,"f"));
+        programme.addActivity(new BasicCourse(5,"g"));
+        programme.addActivity(new BasicCourse(5,"h"));
+
+        programme.addActivity(new BasicCourse(5,"aa"));
+        programme.addActivity(new BasicCourse(5,"bb"));
+        programme.addActivity(new BasicCourse(5,"cc"));
+        programme.addActivity(new BasicCourse(5,"dd"));
+        programme.addActivity(new BasicCourse(5,"ee"));
+        programme.addActivity(new BasicCourse(5,"ff"));
+        programme.addActivity(new BasicCourse(5,"gg"));
+        programme.addActivity(new BasicCourse(5,"hh"));
+
+        programme.addActivity(new BasicCourse(5,"aaa"));
+        programme.addActivity(new BasicCourse(5,"bbb"));
+
+        programme.addActivity(new BasicProject("aaaa"));
+        programme.addActivity(new BasicProject("bbbb"));
+        programme.addActivity(new BasicProject("cccc"));
+
+        programme.addActivity(new SubejctModuleProject("aaaaa"));
+        programme.addActivity(new SubejctModuleProject("bbbbb"));
+
+        programme.addActivity(new BachelorProject("aaaaaa"));
+        assertFalse(programme.valid());
+    }
+
+    @Test
+    void AllSubModCourses() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+        programme.addActivity(new SubjectModuleCourse(5,"a"));
+        programme.addActivity(new SubjectModuleCourse(5,"b"));
+        programme.addActivity(new SubjectModuleCourse(5,"c"));
+        programme.addActivity(new SubjectModuleCourse(5,"d"));
+        programme.addActivity(new SubjectModuleCourse(5,"e"));
+        programme.addActivity(new SubjectModuleCourse(5,"f"));
+        programme.addActivity(new SubjectModuleCourse(5,"g"));
+        programme.addActivity(new SubjectModuleCourse(5,"h"));
+
+        programme.addActivity(new SubjectModuleCourse(5,"aa"));
+        programme.addActivity(new SubjectModuleCourse(5,"bb"));
+        programme.addActivity(new SubjectModuleCourse(5,"cc"));
+        programme.addActivity(new SubjectModuleCourse(5,"dd"));
+        programme.addActivity(new SubjectModuleCourse(5,"ee"));
+        programme.addActivity(new SubjectModuleCourse(5,"ff"));
+        programme.addActivity(new SubjectModuleCourse(5,"gg"));
+        programme.addActivity(new SubjectModuleCourse(5,"hh"));
+
+        programme.addActivity(new SubjectModuleCourse(5,"aaa"));
+        programme.addActivity(new SubjectModuleCourse(5,"bbb"));
+
+        programme.addActivity(new BasicProject("aaaa"));
+        programme.addActivity(new BasicProject("bbbb"));
+        programme.addActivity(new BasicProject("cccc"));
+
+        programme.addActivity(new SubejctModuleProject("aaaaa"));
+        programme.addActivity(new SubejctModuleProject("bbbbb"));
+
+        programme.addActivity(new BachelorProject("aaaaaa"));
+        assertFalse(programme.valid());
+    }
+
+    @Test
+    void AllElectiveCourses() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+        programme.addActivity(new ElectiveCourse(5,"a"));
+        programme.addActivity(new ElectiveCourse(5,"b"));
+        programme.addActivity(new ElectiveCourse(5,"c"));
+        programme.addActivity(new ElectiveCourse(5,"d"));
+        programme.addActivity(new ElectiveCourse(5,"e"));
+        programme.addActivity(new ElectiveCourse(5,"f"));
+        programme.addActivity(new ElectiveCourse(5,"g"));
+        programme.addActivity(new ElectiveCourse(5,"h"));
+
+        programme.addActivity(new ElectiveCourse(5,"aa"));
+        programme.addActivity(new ElectiveCourse(5,"bb"));
+        programme.addActivity(new ElectiveCourse(5,"cc"));
+        programme.addActivity(new ElectiveCourse(5,"dd"));
+        programme.addActivity(new ElectiveCourse(5,"ee"));
+        programme.addActivity(new ElectiveCourse(5,"ff"));
+        programme.addActivity(new ElectiveCourse(5,"gg"));
+        programme.addActivity(new ElectiveCourse(5,"hh"));
+
+        programme.addActivity(new ElectiveCourse(5,"aaa"));
+        programme.addActivity(new ElectiveCourse(5,"bbb"));
+
+        programme.addActivity(new BasicProject("aaaa"));
+        programme.addActivity(new BasicProject("bbbb"));
+        programme.addActivity(new BasicProject("cccc"));
+
+        programme.addActivity(new SubejctModuleProject("aaaaa"));
+        programme.addActivity(new SubejctModuleProject("bbbbb"));
+
+        programme.addActivity(new BachelorProject("aaaaaa"));
+        assertFalse(programme.valid());
+    }
 }
