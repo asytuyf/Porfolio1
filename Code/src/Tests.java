e7c569c05aaa5ccdc4bc0545de3609db4d4c036a
asytuyf <nabaoui21@gmail.com>
2024-03-06 11:31:11 +0100

    2 new tests

--- Code/src/Tests.java
+++ Code/src/Tests.java
@@ -2,23 +2,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//        From here on, everything other than what is mentioned should be valid.
-//
-//        No projects -> invalid
-//        No courses  -> invalid
-//
-//        All below should have the correct amount of ECTs:
-//
-//        Not enough courses  -> invalid
-//
-//        No basic courses -> invalid
-//        No subject mod courses -> invalid
-//        No Elective courses -> invalid
-//
-//        No basic projects -> invalid
-//        No subject mod projects -> invalid
-//        No bachelor project -> invalid
 
 public class Tests {
     @Test
@@ -538,5 +521,44 @@ public class Tests {
 
         programme.addActivity(new BachelorProject("aaaaaa"));
         assertFalse(programme.valid());
+    }
+    @Test
+    void noProjects() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+        programme.addActivity(new BasicCourse(5,"a"));
+        programme.addActivity(new BasicCourse(5,"b"));
+        programme.addActivity(new BasicCourse(5,"c"));
+        programme.addActivity(new BasicCourse(5,"d"));
+        programme.addActivity(new BasicCourse(5,"e"));
+        programme.addActivity(new BasicCourse(5,"f"));
+        programme.addActivity(new BasicCourse(5,"g"));
+        programme.addActivity(new BasicCourse(5,"h"));
+
+        programme.addActivity(new SubjectModuleCourse(5,"aa"));
+        programme.addActivity(new SubjectModuleCourse(5,"bb"));
+        programme.addActivity(new SubjectModuleCourse(5,"cc"));
+        programme.addActivity(new SubjectModuleCourse(5,"dd"));
+        programme.addActivity(new SubjectModuleCourse(5,"ee"));
+        programme.addActivity(new SubjectModuleCourse(5,"ff"));
+        programme.addActivity(new SubjectModuleCourse(5,"gg"));
+        programme.addActivity(new SubjectModuleCourse(5,"hh"));
+
+        programme.addActivity(new ElectiveCourse(5,"aaa"));
+        programme.addActivity(new ElectiveCourse(5,"bbb"));
+        assertFalse(programme.valid());
+    }
+    @Test
+    void noCourses() throws Exception {
+        BachelorProgramme programme = new BachelorProgramme();
+
+        programme.addActivity(new BasicProject("aaaa"));
+        programme.addActivity(new BasicProject("bbbb"));
+        programme.addActivity(new BasicProject("cccc"));
+
+        programme.addActivity(new SubejctModuleProject("aaaaa"));
+        programme.addActivity(new SubejctModuleProject("bbbbb"));
+
+        programme.addActivity(new BachelorProject("aaaaaa"));
+        assertTrue(programme.valid());
     }
 }
